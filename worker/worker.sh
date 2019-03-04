ecab713e2bea8839c9af98acc38031926eaac319
David Salgado <david.salgado@digital.justice.gov.uk>
2019-03-04 15:55:47 +0000

    Simplify DATABASE_URL
    Instead of having two env. vars, DATABASE_URL and
    DATABASE, merge them into a single variable, 
    DATABASE_URL, and stop using DATABASE

--- worker/worker.sh
+++ worker/worker.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 while true; do
-  conn=${DATABASE_URL}/${DATABASE}
+  conn=${DATABASE_URL}
   updated_at=$(date)
   sql="UPDATE dummies SET message='Message updated at: ${updated_at}'"
   psql ${conn} -c "${sql}"
